de17b005ffe00157a15e5d2b1b61f7078b908860
regisfaria <rgisfaria@yahoo.com.br>
2021-03-09 10:15:11 -0300

    finished this module

--- 1#-fundamentals/src/index.js
+++ 1#-fundamentals/src/index.js
@@ -3,14 +3,20 @@ const express = require('express');
 const app = express();
 
 app.get('/courses', (request, response) => {
+  const query = request.query;
+  console.log(query);
   return response.json(['Course 1', 'Course 2', 'Course 3']);
 });
 
 app.post('/courses', (request, response) => {
+  const body = request.body;
+  console.log(body);
   return response.json(['Course 1', 'Course 2', 'Course 3', 'Course 4']);
 });
 
 app.put('/courses/:id', (request, response) => {
+  const { id } = request.params;
+  console.log(id);
   return response.json(['Course 6', 'Course 2', 'Course 3', 'Course 4']);
 })
 
