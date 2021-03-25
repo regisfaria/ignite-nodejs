040759e5e1f15ae81279b145fab1a9bb8af90037
regisfaria <rgisfaria@yahoo.com.br>
2021-03-25 08:56:18 -0300

    renamed

---
+++ modules/4#-rentx-api/src/modules/cars/repositories/implementations/SpecificationRepository.ts
@@ -0,0 +1,45 @@
+import { Specification } from '../../model/Specification';
+import {
+  ICreateSpecificationDTO,
+  ISpecificationRepository,
+} from '../ISpecificationRepository';
+
+class SpecificationRepository implements ISpecificationRepository {
+  private specifications: Specification[];
+
+  private static INSTANCE: SpecificationRepository;
+
+  private constructor() {
+    this.specifications = [];
+  }
+
+  public static getInstance(): SpecificationRepository {
+    if (!SpecificationRepository.INSTANCE) {
+      SpecificationRepository.INSTANCE = new SpecificationRepository();
+    }
+
+    return SpecificationRepository.INSTANCE;
+  }
+
+  findByName(name: string): Specification {
+    const specificationToFind = this.specifications.find(
+      specification => specification.name === name,
+    );
+
+    return specificationToFind;
+  }
+
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+}
+
+export { SpecificationRepository };
